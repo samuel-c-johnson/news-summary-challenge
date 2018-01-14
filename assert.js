1317cf1a369a7fc7716f3f6978d87757a0c8ad9c
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-14 16:55:42 +0000

    Set up test.html page for tests

---
+++ assert.js
@@ -0,0 +1,10 @@
+var assert = {
+
+  isTrue: function(assertionToCheck, testName) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + testName + " is not truthy");
+    } else {
+        console.log(testName + " passing")
+      }
+  }
+};
